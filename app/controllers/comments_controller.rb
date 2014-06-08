e3ce76f509746a3c98e5075f27b3396b0a7f54a0
Nadia Odunayo <nodunayo@gmail.com>
2014-06-08 09:32:55 +0100

    Restrict the ability to add tags

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,6 +31,10 @@ class CommentsController < ApplicationController
     if cannot?(:"change states", @ticket.project)
       params[:comment].delete(:state_id)
     end
+
+    if cannot?(:tag, @ticket.project)
+      params[:comment].delete(:tag_names)
+    end
   end
 
 end
